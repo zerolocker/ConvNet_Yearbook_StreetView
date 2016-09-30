017f7e80bc7b9c1c140cdb4f44bf44412b88056f
Zhuode Liu <zhuode93@cs.utexas.edu>
2016-09-30 00:41:52 -0500

    change cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH -J deeplearnproj1           # Job name
 #SBATCH -o experiment.out    # Specify stdout output file (%j expands to jobId)
-#SBATCH -p gpu                           # Queue name
+#SBATCH -p vis                           # Queue name
 #SBATCH -N 1                     # Total number of nodes requested (16 cores/node)
 #SBATCH -n 1                     # Total number of tasks
 #SBATCH -t 3:30:00              # Run time (hh:mm:ss) - 3.5 hours
 #SBATCH -A CS395T         # Specify allocation to charge against
 
 
-python runvgg.py --learning_rate $lr --eps $eps
+python runvgg.py --learning_rate $lr --eps $eps 2>&1 >lr.${lr}.eps.${eps}.out
